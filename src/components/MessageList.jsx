10f49664782e3ccf4b7dad4c0d84f114f49d750d
ridwanfansuri <ridwan@icehousecorp.com>
2016-11-02 17:52:26 +0700

    add firebase

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Message from './Message.jsx';
 import mui from 'material-ui';
+import firebase from 'firebase'
+import _ from 'lodash';
 
 var {Card,List} = mui;
 
@@ -8,17 +10,36 @@ class MessageList extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      messages: [
-        'heloo there how are you',
-        'I am fine, and you?'
-      ]
+      messages: []
     };
+  //   this.firebaseRef = new Firebase('https://react-stack-65c96.firebaseio.com/messages');
+  //   this.firebaseRef.once("value", (dataSnapshot) => {
+  //     var message = dataSnapshot.val();
+  //     console.log(message);
+  //     this.setState({
+  //       messages: messages
+  //     });
+  //   })
   }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const messageRef = rootRef.child('message');
+    messageRef.on('value', snap => {
+      this.setState({
+            messages: snap.val()
+          });
+    })
+  }
+
+
+
+
 
   render(){
     var messageNodes = this.state.messages.map((message)=> {
       return (
-          <Message message={message} />
+          <Message message={message.message} />
       );
     })
     return (
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import App from './components/App.jsx';
 require('./main.scss');
-
+import firebase from 'firebase'
+// Initialize Firebase
+let config = {
+  apiKey: "AIzaSyBzqAYu5xPu8xmYdboiQW9kPk1p0QHBE4k",
+  authDomain: "react-stack-65c96.firebaseapp.com",
+  databaseURL: "https://react-stack-65c96.firebaseio.com",
+  storageBucket: "react-stack-65c96.appspot.com",
+  messagingSenderId: "298137980517"
+};
+firebase.initializeApp(config);
 React.render(<App />, document.getElementById('container'));
