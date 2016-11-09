fca678a9a9be0452c63fc3bb4398e75b5c966144
ridwanf <ridwanfansuri@gmail.com>
2016-11-09 21:44:00 +0700

    edit authentication method

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,9 @@ import alt from '../alt';
 class Actions {
   login(args){
     return (dispatch) => {
+      debugger;
       var provider = new firebase.auth.GoogleAuthProvider();
-      provider.addScope('profile');
-      provider.addScope('email');
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
       firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token.
         var token = result.credential.accessToken;
@@ -13,6 +13,9 @@ class Actions {
         var user = result.user;
         dispatch(user);
       }).catch(function (error) {
+        var errorCode = error.code;
+        var erorMessage = error.message;
+        console.log(error);
         return;
       });
       // firebase.authWithOAuthPopup("google", (error, user)=> {
